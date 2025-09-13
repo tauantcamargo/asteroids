27b42bd1e3c6074e5b84200b0901ad3f56e81adf
tauantcamargo <lukyhenson@gmail.com>
2025-09-13 20:39:27 -0300

    feat: add colliding check method

--- circleshape.py
+++ circleshape.py
@@ -17,3 +17,7 @@ class CircleShape(pygame.sprite.Sprite):
 
     def update(self, dt):
         pass
+
+    def is_colliding(self, other):
+        distance = self.position.distance_to(other.position)
+        return distance < self.radius + other.radius
