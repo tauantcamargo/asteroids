119dd5ea3d5ec41532a6b09814523f27f1f02e3d
tauantcamargo <lukyhenson@gmail.com>
2025-09-13 19:46:15 -0300

    feat: add groups to update and be drawable

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player import Player
 import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
@@ -6,13 +7,32 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+
+        updatable.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock().tick(60) / 1000.0
 
 
 if __name__ == "__main__":
