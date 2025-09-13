1c48ea7abd2bb2e424dbe32e4c0c9b813e844bc4
tauantcamargo <lukyhenson@gmail.com>
2025-09-13 18:57:14 -0300

    feat: opening black screen

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
